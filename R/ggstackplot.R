81881be32ca99193e26e86aae6b723ad00705604
Sebastian Kopf <sebastian.kopf@colorado.edu>
2023-06-07 00:28:51 -0600

    bare bones prep data function

--- R/ggstackplot.R
+++ R/ggstackplot.R
@@ -9,33 +9,70 @@ ggstackplot <- function(data, x, y, direction = c("guess", "horizontal", "vertic
 }
 
 prepare_data <- function(data, x, y, direction = c("guess", "horizontal", "vertical")) {
-
   # do we have a data frame?
-  if (missing(data) || !is.data.frame(data))
+  if (missing(data) || !is.data.frame(data)) {
     abort("`data` must be a data frame or tibble.")
+  }
 
-  # do we have value x and y
+  # do x and y evaluate correctly?
   x <- try_fetch(
-    tidyselect::eval_select(rlang::enexpr(x), data) |> names(),
-    error = function(cnd) abort(
-      "`x` must be a valid tidyselect expression.",
-      parent = cnd
-    ))
+    tidyselect::eval_select(rlang::enexpr(x), data),
+    error = function(cnd) {
+      abort(
+        "`x` must be a valid tidyselect expression.",
+        parent = cnd
+      )
+    }
+  )
   y <- try_fetch(
-    tidyselect::eval_select(rlang::enexpr(y), data) |> names(),
-    error = function(cnd) abort(
-      "`y` must be a valid tidyselect expression.",
-      parent = cnd
+    tidyselect::eval_select(rlang::enexpr(y), data),
+    error = function(cnd) {
+      abort(
+        "`y` must be a valid tidyselect expression.",
+        parent = cnd
+      )
+    }
+  )
+
+  # do we have at least 1 x and 1 y?
+  if (length(x) < 1 || length(y) < 1) {
+    abort(c(
+      "insufficient number of columns",
+      "x" = if (length(x) < 1) "no `x` column selected",
+      "x" = if (length(y) < 1) "no `y` column selected"
     ))
+  }
 
-  print(x)
-  print(y)
+  # do we have a valid direction picked?
+  direction <- arg_match(direction)
 
-   #direction <- match.arg(direction)
+  # are the direction and number of columns consistent
+  if (direction == "horizontal" && length(y) > 1) {
+    abort("`y` must be just 1 column for a horizontally stacked plot")
+  } else if (direction == "vertical" && length(x) > 1) {
+    abort("`x` must be just 1 column for a vertically stacked plot")
+  } else if (direction == "guess" && length(x) > 1 && length(y) > 1) {
+    abort("either `x` or `y` must be just 1 column for guessing the stacking direction")
+  }
 
-  #if(direction == "x") stop("not implemented yet", call. = FALSE)
+  # guess direction
+  if (direction == "guess") direction <- if (length(x) > 1) "horizontal" else "vertical"
 
+  # prep data (pivot based on direction)
+  data_long <-
+    if (direction == "horizontal") {
+      data |>
+        dplyr::rename(dplyr::all_of(x), dplyr::all_of(y)) |>
+        tidyr::pivot_longer(cols = dplyr::all_of(names(x))) |>
+        dplyr::mutate(.x = .data$value, .y = !!sym(names(!!y)[1]))
+    } else {
+      data |>
+        dplyr::rename(dplyr::all_of(x), dplyr::all_of(y)) |>
+        tidyr::pivot_longer(cols = dplyr::all_of(names(y))) |>
+        dplyr::mutate(.y = .data$value, .x = !!sym(names(!!x)[1]))
+    }
 
+  return(data_long)
 }
 
 prepare_plots <- function() {
--- tests/testthat/test-ggstackplot.R
+++ tests/testthat/test-ggstackplot.R
@@ -2,9 +2,46 @@ context("ggstackplot")
 
 
 test_that("prepare_data() tests", {
-
-  expect_error(prepare_data(), "`data` must be a data frame or tibble")
-  expect_error(prepare_data(mtcars, x=dne), "`x` must be a valid tidyselect expression")
-  expect_error(prepare_data(mtcars, x=mpg, y=dne), "`y` must be a valid tidyselect expression")
+  # parameter checks
+  expect_error(
+    prepare_data(),
+    "`data` must be a data frame or tibble"
+  )
+  expect_error(
+    prepare_data(mtcars),
+    "insufficient number of columns"
+  )
+  expect_error(
+    prepare_data(mtcars, x = DNE),
+    "`x` must be a valid tidyselect expression"
+  )
+  expect_error(
+    prepare_data(mtcars, x = mpg),
+    "insufficient number of columns"
+  )
+  expect_error(
+    prepare_data(mtcars, x = mpg, y = DNE),
+    "`y` must be a valid tidyselect expression"
+  )
+  expect_error(
+    prepare_data(mtcars, y = mpg),
+    "insufficient number of columns"
+  )
+  expect_error(
+    prepare_data(mtcars, x = mpg, y = disp, direction = "DNE"),
+    "`direction` must be one of"
+  )
+  expect_error(
+    prepare_data(mtcars, x = mpg, y = c(disp, wt), direction = "horizontal"),
+    "`y` must be just 1 column"
+  )
+  expect_error(
+    prepare_data(mtcars, x = c(mpg, wt), y = disp, direction = "vertical"),
+    "`x` must be just 1 column"
+  )
+  expect_error(
+    prepare_data(mtcars, x = c(mpg, wt), y = c(disp, qsec)),
+    "either `x` or `y` must be just 1 column"
+  )
 
 })
