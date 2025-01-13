6e301e024a094722abcca29503b1aad21c5e47e7
Sebastian Kopf <sebastian.kopf@colorado.edu>
2025-01-13 10:09:52 +0100

    add cli imports and switch abort to cli_abort

--- R/ggstackplot-package.R
+++ R/ggstackplot-package.R
@@ -1,10 +1,14 @@
-#' @keywords internal
-#' @aliases ggstackplot-package
-"_PACKAGE"
-
 ## usethis namespace: start
 #' @import rlang
+#' @import cli
 #' @import ggplot2
 #' @importFrom lifecycle deprecated
 ## usethis namespace: end
 NULL
+
+#' @details
+#' `r lifecycle::badge("stable")`
+#'
+#' Have you ever wanted to compare multiple panels of line plots that do not share coordinate schemes? These kinds of plots are ubiquitous in the Earth sciences, but there is not an easy way to create them with ggplot facets. Check out the functionality provided by ggstackplots with useful examples and plot styling ideas at <https://ggstackplot.kopflab.org>
+#'
+"_PACKAGE"
